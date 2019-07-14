90f4ea6ccdca277691befec1b51a7124bc191746
Drew Lanning <dance.monkey@gmail.com>
2019-07-14 09:44:28 -0400

    added color list and instructions to various help and settings files adjusted bad html tags in help file that made one section too tiny to read made change to watch IC to try and fix issue with populated data disappearing when first opening app

--- Conveyor/Onboarding/OnboardingVC.swift
+++ Conveyor/Onboarding/OnboardingVC.swift
@@ -42,7 +42,7 @@ class OnboardingVC: UIPageViewController {
       ("Swipe for actions", "Swipe right to complete, swipe left to see other task actions like delete and reschedule."),
       ("Tasks reschedule themselves daily", #"At the first launch each day, tasks change lists. Later tasks move to Tomorrow, Tomorrow tasks move to Today."#),
       ("Lock tasks in the Later list", #"A “Later” task can be locked in place, so it remains there for as long as you like."#),
-      ("Special features for PRO users", #"Use the "@!" tag in your tasks to make it high-priority, or "@rpt" to make a daily repeating task."#),
+      ("Special features for PRO users", #"Use the "@!" tag in your tasks to make it high-priority,  "@rpt" to make a daily repeating task, or "@blue" (other colors available) to tag tasks with a color."#),
       ("You're ready to get started!", #"We're just going to ask for a couple of authorizations, so that you can see badges for what's due today and use Siri to add tasks."#)
     ]
     screenshots = [
